9100018dca10503ef597e06e6574928a4216277f
Yann Voté <ygversil@lilo.org>
2019-06-09 19:34:49 +0200

    Update translations

--- i18n/csv_tools_fr.ts
+++ i18n/csv_tools_fr.ts
@@ -3,9 +3,30 @@
 <context>
     <name>@default</name>
     <message>
-        <location filename="test_translations.py" line="48"/>
+        <location filename="../test/test_translations.py" line="48"/>
         <source>Good morning</source>
         <translation>Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>CSVToolsAlgorithm</name>
+    <message>
+        <location filename="../csv_tools_algorithm.py" line="70"/>
+        <source>Input layer</source>
+        <translation>Couche en entrée</translation>
+    </message>
+    <message>
+        <location filename="../csv_tools_algorithm.py" line="81"/>
+        <source>Output layer</source>
+        <translation>Couche en sortie</translation>
+    </message>
+</context>
+<context>
+    <name>CSVToolsProvider</name>
+    <message>
+        <location filename="../csv_tools_provider.py" line="78"/>
+        <source>CSV Tools</source>
+        <translation>Outils CSV</translation>
     </message>
 </context>
 </TS>
